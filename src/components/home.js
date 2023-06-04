196b5a09fd8e624a99cd4466d24dcc50911bec7f
abdi <126338923+abmu1@users.noreply.github.com>
2023-06-04 08:55:31 +0300

    add simple home page

---
+++ src/components/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome To Super Electronics</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio odio suscipit sunt consectetur optio velit
+        eaque praesentium iure dolore mollitia delectus provident magnam corrupti, dicta quisquam sequi? At, est ad.
+        Unde repellendus labore consectetur fugit ab reiciendis expedita ipsum error distinctio velit similique odit ratione,
+        aspernatur tenetur culpa aliquam debitis saepe quae nemo magnam laboriosam! Ipsam optio architecto vero delectus?
+        Ullam harum, mollitia praesentium sit consequuntur odio rem est cupiditate, tempora, beatae aspernatur alias ipsam quam
+        cumque minima voluptatem soluta laborum dolore quas labore exercitationem facilis aut. Soluta, reiciendis expedita.
+      </p>
+    </div>
+  )
+}
+
+export default Home;
