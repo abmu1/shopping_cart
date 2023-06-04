b57dcbab9184b191eb7c1836bc4fa7356456ae31
abdi <126338923+abmu1@users.noreply.github.com>
2023-06-04 16:23:29 +0300

    add catalog items to cart

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -1,8 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import uniqid from 'uniqid';
 
-function Catalog() {
+function Catalog({setCart}) {
   const [products, setProducts] = useState([]);
+
+  const addToCart = (e) => {
+    const id = +e.currentTarget.id;
+    const product = products.filter(product => {
+      return (product.id === id);
+    })[0];
+
+    setCart(prevCart => {
+      let item = null;
+      const newCart = prevCart.filter(prevItem => {
+        if (prevItem.id === id) {
+          item = {...prevItem};
+          return false;
+        }
+        return true;
+      });
+
+      if (item) {
+        item.quantity += 1;
+      }
+      else{
+        item = {...product, quantity: 1};
+      }
+      newCart.push(item);
+      return newCart;
+    });
+  };
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
@@ -11,9 +38,12 @@ function Catalog() {
     })
     .then(products => {
       setProducts(products);
-    }
-    )
+    })
+    .catch(err => {
+      console.log(err)
+    })
   },[]);
+
   return (
     <div>
       <h2>Browse Catalog</h2>
@@ -24,7 +54,7 @@ function Catalog() {
               <img src={product.image} alt='product'/>
               <p>{product.title}</p>
               <p>{product.price}</p>
-              <button>Add to cart</button>
+              <button onClick={addToCart} id={product.id}>Add to cart</button>
             </div>
           )
         })}
