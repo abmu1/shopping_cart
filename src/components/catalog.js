9488b0276ba1e142e12396771cce69f52f72d811
abdi <126338923+abmu1@users.noreply.github.com>
2023-06-05 08:36:44 +0300

    add category filter

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -3,6 +3,11 @@ import uniqid from 'uniqid';
 
 function Catalog({setCart}) {
   const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState('All');
+
+  const changeCategory = (e) => {
+    setCategory(e.currentTarget.value);
+  };
 
   const addToCart = (e) => {
     const id = +e.currentTarget.id;
@@ -47,8 +52,20 @@ function Catalog({setCart}) {
   return (
     <div>
       <h2>Browse Catalog</h2>
+      <label>
+        Choose Category
+        <select onInput={changeCategory}>
+          <option value='All'>All</option>
+          <option value="men's clothing">Men's Clothing</option>
+          <option value="jewelery">Jewelery</option>
+          <option value="electronics">Electronics</option>
+          <option value="women's clothing">Women's Clothing</option>
+        </select>
+      </label>
       <div>
-        {products.map(product => {
+        {products.filter((product) => {
+          return (product.category === category || (category === 'All'));;
+        }).map(product => {
           return (
             <div key={uniqid()}>
               <img src={product.image} alt='product'/>
